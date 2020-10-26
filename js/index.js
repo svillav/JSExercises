43089e352e3e56474beeda58bc62a33637abbf5e
Santiago Villa <santiago.villa@radiumrocket.com>
2020-10-26 13:26:34 -0300

    JS: Task 01. Exercise 1 a.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// 1) Variables y Operadores
+
+// a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la
+//  suma de ambos números en una 3er variable.
+
+var num1 = 5;
+var num2 = 2;
+var sum = num1 + num2;
+console.log('Sum: ', sum);
