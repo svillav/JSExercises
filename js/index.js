ab1f2bda3b3a9fe4b819af73ac22f19d5c797bff
Santiago Villa <santiago.villa@radiumrocket.com>
2020-10-26 13:26:52 -0300

    JS: Task 01. Exercise 1 b.

--- js/index.js
+++ js/index.js
@@ -7,3 +7,11 @@ var num1 = 5;
 var num2 = 2;
 var sum = num1 + num2;
 console.log('Sum: ', sum);
+
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er
+//  variable.
+
+var str1 = 'John ';
+var str2 = 'Doe';
+var concat = str1 + str2;
+console.log('Concat: ', concat);
