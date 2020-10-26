5d059db749f2ca6f1528a854fbd7ad191a9aa0ca
Santiago Villa <santiago.villa@radiumrocket.com>
2020-10-26 13:53:03 -0300

    JS: Task 01. Exercise 2 b.

--- js/index.js
+++ js/index.js
@@ -36,3 +36,11 @@ console.log('Str Lenght: ', strLength);
 var str5 = 'javascript';
 var upper = str5.toUpperCase();
 console.log('Upper: ', upper);
+
+// b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
+//  con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar
+//  substring).
+
+var str6 = 'Fullstack developer';
+var substr = str6.substring(0, 5);
+console.log('Substring: ', substr);
