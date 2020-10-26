7e3cdeb57e33ca444d898741c25cc6c733ac453f
Santiago Villa <santiago.villa@radiumrocket.com>
2020-10-26 16:43:54 -0300

    JS: Task 01. Exercise 2 f.

--- js/index.js
+++ js/index.js
@@ -68,3 +68,16 @@ console.log('Substring 3: ', substr3);
 var str9 = 'Use IndexOf';
 var spaceIndex = str9.indexOf(' ');
 console.log('Space Index: ', spaceIndex);
+
+// f) Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún
+//  espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un
+//  nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás
+//  letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el
+//  operador +).
+
+var str10 = 'last substring';
+var substr4 = str10.substring(0, 1).toUpperCase() +
+    str10.substring(1, 5).toLowerCase() +
+    str10.substring(5, 6).toUpperCase() +
+    str10.substring(6).toLowerCase();
+console.log('Last substring: ', substr4);
